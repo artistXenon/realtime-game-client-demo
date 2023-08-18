07cdd96bcf8c14150fab4665294faa6600ceeaf2
artistXenon <xtramainpp@gmail.com>
2023-08-18 17:19:38 +0900

    unify whitespace

--- src/main.ts
+++ src/main.ts
@@ -29,9 +29,9 @@ Global.PointerEventGroup.registerEvent();
 Global.PointerEventGroup.setListenSequenceFirstInFirstTrigger(false);
 
 assets.onLoad = () => {
-  Global.FontPoppin = new FontBuilder("Poppin");
-  Global.FontQuicksand = new FontBuilder("Quicksand");
-  setTimeout(() => scene.showAilre(), 1000);
+    Global.FontPoppin = new FontBuilder("Poppin");
+    Global.FontQuicksand = new FontBuilder("Quicksand");
+    setTimeout(() => scene.showAilre(), 1000);
 };
 
 
@@ -44,13 +44,13 @@ const ipcInterface = (<any>window).electronIPC;
 
 // listen to node
 ipcInterface.showedTime((e: IpcRendererEvent, c: number) => {
-  console.log(c + 1);
-  setTimeout(() => ipcInterface.showTime(c + 1), 500);
+    console.log(c + 1);
+    setTimeout(() => ipcInterface.showTime(c + 1), 500);
 });
 
 
 ipcInterface.onError((e: IpcRendererEvent, err: string) => {
-  console.log(err);
+    console.log(err);
 });
 
 // setTimeout(() => {
@@ -62,12 +62,12 @@ ipcInterface.onError((e: IpcRendererEvent, err: string) => {
 //
 
 function onResize() {  
-  const engine = Global.Engine;
-  engine.resizeCanvas();
-  ResolutionVector2D.baseVector.X = engine.Canvas.width;
-  ResolutionVector2D.baseVector.Y = engine.Canvas.height;
-  if (engine.Scene instanceof Sprite) {
-    engine.Scene.Width = engine.Canvas.width;
-    engine.Scene.Height = engine.Canvas.height;
-  }
+    const engine = Global.Engine;
+    engine.resizeCanvas();
+    ResolutionVector2D.baseVector.X = engine.Canvas.width;
+    ResolutionVector2D.baseVector.Y = engine.Canvas.height;
+    if (engine.Scene instanceof Sprite) {
+        engine.Scene.Width = engine.Canvas.width;
+        engine.Scene.Height = engine.Canvas.height;
+    }
 }
